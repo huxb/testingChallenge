8653a050b946666a5eb57acb613112e8d1ddb7dd
Aayush <aayush19saxena@gmail.com>
2016-07-16 00:09:15 -0700

    Added birthday tests

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -8,34 +8,15 @@ describe('Last Name', function () {
         var lastNameInput = element(by.css('#lastname'));
         // if the last name bar has been touched, but still no input, 
         // there should be error message dislayed
-        lastNameInput.sendKeys(' ');
+        lastNameInput.sendKeys("");
         var errorMessage = element(by.css('#vlastname'));
-        expect(errorMessage.isDisplayed()).toEqual(true);
+        expect(errorMessage.isPresent()).toEqual(true);
 
         // if the last name bar has been touched, and has an input, 
         // there should not be error message dislayed
         lastNameInput.clear();
         lastNameInput.sendKeys('a');
         expect(errorMessage.isDisplayed()).toEqual(false);
-    });
-});
-
-//Test password and confirm passowrd for match
-describe('Password', function(){
-    browser.get('http://localhost:8080/#/signup-form.html');
-    var password = element(by.css('#pasword'));
-    var confirmPass = element(by.css('#confirm'));
-    it('password1 should exist', function() {
-        expect(password).not.toBe("");
-        expect(element(by.css('#enterPass1')).isDisplayed()).toBe(true);
-
-        expect(confirmPass).not.toBe("");
-        expect(element(by.css('#enterPass2')).isDisplayed()).toBe(true);
-    });
-
-    it('both passwords should match', function(){
-        expect(password).toBe(confirmPass);    
-        expect(element(by.css('.matchPass')).isDisplayed()).toBe(true);
     });
 });
 
@@ -61,5 +42,14 @@ describe('Email', function() {
         email.sendKeys('validEmail@testing.app');
         expect(element(by.css('#emptyEmail')).isDisplayed()).toEqual(false);
         expect(element(by.css('#invalidEmail')).isDisplayed()).toEqual(false);
+    });
+});
+
+describe('Birthdate', function() {
+    it('should give invalid error when a user is under 13', function() {
+        var birthdate = element(by.css('#birthdate')); 
+        var check = element(by.css('#birthdate'));
+        birthdate.sendKeys('1/1/2014');
+        expect(check.isPresent()).toEqual(true);
     });
 });
