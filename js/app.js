dd0d99ee6a8866a79342b112326dbea1c49016b9
Aayush <aayush19saxena@gmail.com>
2016-07-15 23:22:14 -0700

    Completed form functionality

--- js/app.js
+++ js/app.js
@@ -3,5 +3,28 @@
 var testingApp = angular.module('TestingApp', []);
 
 testingApp.controller('testingCtrl', ['$scope', function($scope) {
-    console.log($scope.email);
+    $scope.validateBday = function() {
+    	$scope.signupForm.birthdate.$setValidity("validDate", true);
+    	$scope.signupForm.birthdate.$setValidity("validAge", true);
+    	var date = Date.parse($scope.birthdate);
+    	if(date.toString() == 'NaN') {
+    		// date is not a number
+    		$scope.signupForm.birthdate.$setValidity("validDate", false);
+    	} else {
+    		if(age < 13) {
+    			$scope.signupForm.birthdate.$setValidity("validAge", false);
+    		}
+    	}
+    }
+    // helper method to get the right date format
+	function getAge(date) {
+	    var today = new Date();
+	    var birthDate = new Date(date);
+	    var age = today.getFullYear() - birthDate.getFullYear();
+	    var m = today.getMonth() - birthDate.getMonth();
+	    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+	        age--;
+	    }    
+	    return age;
+	}
 }]);
